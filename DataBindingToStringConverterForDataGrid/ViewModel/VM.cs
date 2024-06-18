5d49b8b6edc2db9a246e3e07107a011fd361943c
Елисей Мартьянов <martyanovelisey@outlook.com>
2024-06-18 18:14:30 +0300

    Нерабочий вариант

--- DataBindingToStringConverterForDataGrid/ViewModel/VM.cs
+++ DataBindingToStringConverterForDataGrid/ViewModel/VM.cs
@@ -13,9 +13,6 @@ namespace DataBindingToStringConverterForDataGrid.ViewModel
         private MyRectangle _Rectangle;
         public MyRectangle Rectangle { get => _Rectangle; set => Set(ref _Rectangle, value); }
 
-        private MyRectangleWithDP _RectangleWithDP;
-        public MyRectangleWithDP RectangleWithDP { get => _RectangleWithDP; set => Set(ref _RectangleWithDP, value); }
-
         public VM()
         {
             Rectangle = new MyRectangle("МойПрямоугольник", 10, 15);
