983cb4f6e1be2490fdc527855f7ac383433361b5
Елисей Мартьянов <martyanovelisey@outlook.com>
2024-06-18 16:27:16 +0300

    Подключена VM

--- DataBindingToStringConverterForDataGrid/View/MainWindow.xaml.cs
+++ DataBindingToStringConverterForDataGrid/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataBindingToStringConverterForDataGrid.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace DataBindingToStringConverterForDataGrid
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
     }
 }
